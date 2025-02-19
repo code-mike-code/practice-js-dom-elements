9ac42f40aadcac825edf6fc7206dd091cde82280
Michał <m.majewski.dm@gmail.com>
2025-02-20 00:04:14 +0100

    task 01 finished

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const commentsElements = document.querySelector('.comments__item.comments__item--newest');
+if(commentsElements) {
+    const dataInfoEl = commentsElements.querySelectorAll('[data-info]');
+    console.log(dataInfoEl.length);
+}
+
