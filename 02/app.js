fd1d7fc14c7b2079487907040af845c5b9456752
Michał <m.majewski.dm@gmail.com>
2025-02-20 00:21:31 +0100

    task 02 finished

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+
+const linksEl = document.querySelectorAll('a[data-url]');
+linksEl.forEach(function(link, index) {
+    const dataUrl = link.getAttribute('data-url');
+    link.setAttribute('href', dataUrl)
+})
