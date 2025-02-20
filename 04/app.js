fd7d012fa58b5f4b04cab79af3f998c1ff083982
Michał <m.majewski.dm@gmail.com>
2025-02-20 23:42:56 +0100

    task 04 finished

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,34 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+
+const navElement = document.querySelector('nav');
+if(navElement) {
+    const ulElement = document.createElement('ul');
+if(ulElement) {
+    menuItems.forEach( function(item) {
+        const liElement = document.createElement('li')
+        const linkElement = document.createElement('a')
+        linkElement.setAttribute('href', item.url)
+        linkElement.innerText = item.text
+        liElement.appendChild(linkElement)
+        ulElement.appendChild(liElement)
+
+    } )
+    
+    
+    // for(let key in menuItems) {
+    //     const liElement = document.createElement('li')
+    //     const linkElement = document.createElement('a')
+    //     linkElement.setAttribute('href', menuItems[key].url)
+    //     linkElement.innerText = menuItems[key].text
+    //     liElement.appendChild(linkElement)
+    //     ulElement.appendChild(liElement)
+    
+    // }
+}
+
+    navElement.appendChild(ulElement)
+}
+
