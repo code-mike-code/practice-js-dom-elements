b13ef45c293bfcc3b760c3d32878196989f8e4ec
Michał <m.majewski.dm@gmail.com>
2025-02-20 01:27:00 +0100

    task 03 finished

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,35 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+
+const buttonSection  = document.querySelector('.parent-for-button')
+
+
+// if(buttonSection) {
+//     const button = document.createElement('button');
+
+//     button.classList.add(buttonSettings.attr.className);
+//     button.title = buttonSettings.attr.title;
+//     button.style.border = buttonSettings.css.border;
+//     button.style.padding = buttonSettings.css.padding;
+//     button.style.color = buttonSettings.css.color;
+//     button.textContent = buttonSettings.text;
+
+//     buttonSection.appendChild(button)
+// }
+
+
+const button = document.createElement('button');
+
+for(let key in buttonSettings.attr) {
+    button.setAttribute(key, buttonSettings.attr[key])
+}
+for(let key in buttonSettings.css) {
+    button.setAttribute(key, buttonSettings.css[key])
+}
+
+button.textContent = buttonSettings.text;
+
+buttonSection.appendChild(button)
+
