a98d4f36e1ba1cb2ee7de2b190a43b77bd07eb90
Michał <m.majewski.dm@gmail.com>
2025-02-21 01:44:56 +0100

    task 05 finished

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,36 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+if(curr) {
+    const buttonsParent = curr.parentElement
+    const nextButton = document.createElement('button');
+
+    nextButton.innerText = 'Usuń z koszyka'
+
+    buttonsParent.appendChild(nextButton);
+
+    for(let i = 0; i < buttonsParent.children.length; i++) {
+        if(buttonsParent.children[i] !== curr) {
+            buttonsParent.children[i].classList.add('.siblings')
+        }
+    }
+
+    buttonsParent.nextElementSibling.setAttribute('title', 'nextElementSibling')
+
+    const buttonsParentParent = buttonsParent.parentElement
+    const lastArticle = buttonsParentParent.lastElementChild
+    const lastButton = lastArticle.querySelector('button')
+    const pEl = document.createElement('p')
+
+    pEl.innerText = 'Paragraf przed buttonem'
+    lastArticle.insertBefore(pEl, lastButton)
+
+
+    const article = buttonsParentParent.querySelector('article')
+    const newArticle = article.cloneNode(true)
+
+    buttonsParentParent.insertBefore(newArticle, buttonsParentParent.firstElementChild)
+
+
+
+}
